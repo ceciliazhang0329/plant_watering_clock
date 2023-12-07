02c532745d39e82f35fe49cb452e2419edb5f27a
ceciliazhang0329 <128743117+ceciliazhang0329@users.noreply.github.com>
2023-12-07 17:54:15 +0000

    add rounts to users

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,8 @@ Rails.application.routes.draw do
 
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  root "plants#index"
   # Defines the root path route ("/")
+  
   # root "articles#index"
 end
